a143ec29d7055a5147e073053fdb4afd0371b7e3
Tim Hourihan <91994391+thourihan@users.noreply.github.com>
2023-12-07 23:42:06 -0500

    Added Grad-CAM

--- run_model.py
+++ run_model.py
@@ -1,47 +1,94 @@
 import torch
-import torch.nn as nn
+import torch.nn as nn 
+import torch.nn.functional as F
 from torchvision import transforms
 from PIL import Image
 from efficientnet_pytorch import EfficientNet
-import torch.nn.functional as F
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2 
 
 
-# Step 1: Initialize your model architecture with EfficientNet b3
+# Initialize model architecture with EfficientNet B3
 model = EfficientNet.from_pretrained('efficientnet-b3')
-
-# Modify the final layer for binary classification
-num_classes = 2  # genuine and deep fake
+num_classes = 2
 in_features = model._fc.in_features
 model._fc = nn.Linear(in_features, num_classes)
-
-# Step 2: Load the saved model weights
 model.load_state_dict(torch.load("model_after_test.pth", map_location=torch.device('cpu')))
-model.eval()  # Set the model to evaluation mode
+model.eval()
 
-# Step 3: Prepare your image
+# Preparing image
 def process_image(image_path):
-    image = Image.open(image_path)
+    image = Image.open(image_path).convert('RGB')
     preprocess = transforms.Compose([
         transforms.Resize(256),
         transforms.CenterCrop(224),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
-    image = preprocess(image)
-    image = image.unsqueeze(0)  # Add a batch dimension
-    return image
+    return preprocess(image).unsqueeze(0)
 
-# Replace 'Biden.jpg' with the path to the image you want to test
-image = process_image("Biden.jpg")
+# Grad-CAM
+class GradCam:
+    def __init__(self, model, layer_name):
+        self.model = model
+        self.layer_name = layer_name
+        self.gradient = None
+        self.register_hooks()
 
-# Step 4: Make a prediction
-with torch.no_grad():
-    prediction = model(image)
-    # Process the prediction as needed
-    logits = model(image)
-    probabilities = F.softmax(logits, dim=1)
-    predicted_class = torch.argmax(probabilities, dim=1)
-    print(f"Raw logits: {logits}")
-    print(f"Probabilities: {probabilities}")
-    print(f"Predicted class: {predicted_class.item()}")
+    def save_gradient(self, grad):
+        self.gradient = grad
 
+    def register_hooks(self):
+        layer = getattr(self.model, self.layer_name)
+
+        def forward_hook(module, input, output):
+            self.feature = output
+
+        def backward_hook(module, grad_in, grad_out):
+            self.save_gradient(grad_out[0])
+
+        layer.register_forward_hook(forward_hook)
+        layer.register_backward_hook(backward_hook)
+
+    def generate_cam(self, input_image, target_class):
+        model_output = self.model(input_image)
+        self.model.zero_grad()
+        one_hot_output = torch.FloatTensor(1, model_output.size()[-1]).zero_()
+        one_hot_output[0][target_class] = 1
+        model_output.backward(gradient=one_hot_output, retain_graph=True)
+
+        gradient = self.gradient[0].cpu().data.numpy()
+        weight = np.mean(gradient, axis=(1, 2))[..., np.newaxis, np.newaxis]
+        feature = self.feature[0].cpu().data.numpy()
+
+        cam = np.sum(weight * feature, axis=0)
+        cam = np.maximum(cam, 0)
+        cam = cam - np.min(cam)
+        cam = cam / np.max(cam)
+        return cam
+
+def show_cam_on_image(img, mask):
+    # Resize mask to match the image size
+    mask = cv2.resize(mask, (img.shape[1], img.shape[0]))
+    heatmap = cv2.applyColorMap(np.uint8(255 * mask), cv2.COLORMAP_JET)
+    heatmap = np.float32(heatmap) / 255
+    cam = heatmap + np.float32(img)
+    cam = cam / np.max(cam)
+    return np.uint8(255 * cam)
+
+
+# Process the image
+img_path = "Biden.jpg"
+img = np.array(Image.open(img_path).convert('RGB'))
+img_tensor = process_image(img_path)
+
+# Grad-CAM
+grad_cam = GradCam(model, '_conv_head')
+target_class = torch.argmax(model(img_tensor)).item()
+cam = grad_cam.generate_cam(img_tensor, target_class)
+cam_image = show_cam_on_image(img / 255.0, cam)
+
+# Display image
+plt.imshow(cam_image)
+plt.show()
